0bea29615ed243ba57fe6ac7aba800fe9503980f
Paul Allen <paul.allen@lithium.com>
2014-06-17 05:30:44 -0500

    implementing source destination checks disablement

--- boot-scripts/disable_source_destination_checks.sh
+++ boot-scripts/disable_source_destination_checks.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-echo "not yet implemented"
-exit 1
+AZ="$(curl -sL instance-data/latest/meta-data/placement/availability-zone)"
+REGION=${AZ%?}
+INSTANCE_ID=$(curl -sL 169.254.169.254/latest/meta-data/instance-id
+
+aws ec2 modify-instance-attribute --region $REGION --instance-id i-$INSTANCE_ID --no-source-dest-check
