ba4ae13cbfde02807bccca34f7a59889696ecec6
Paul Allen <paul.allen@lithium.com>
2014-06-24 05:28:39 -0500

    fixing scripts

--- boot-scripts/disable_source_destination_checks.sh
+++ boot-scripts/disable_source_destination_checks.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-AZ="$(curl -sL instance-data/latest/meta-data/placement/availability-zone)"
+AZ="$(curl -sL 169.254.169.254/latest/meta-data/placement/availability-zone)"
 REGION="${AZ%?}"
 INSTANCE_ID="$(curl -sL 169.254.169.254/latest/meta-data/instance-id)"
 
-aws ec2 modify-instance-attribute --region $REGION --instance-id i-$INSTANCE_ID --no-source-dest-check
+aws ec2 modify-instance-attribute --region $REGION --instance-id i-$INSTANCE_ID --source-dest-check '{"Value": true}'
