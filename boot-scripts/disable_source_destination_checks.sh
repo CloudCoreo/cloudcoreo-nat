ec7270a32b7e037a5c2f4c078574e97a04e365fe
Paul Allen <paul.allen@lithium.com>
2014-07-12 11:11:02 -0500

    adding the ha-nat installation

--- boot-scripts/disable_source_destination_checks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-AZ="$(curl -sL 169.254.169.254/latest/meta-data/placement/availability-zone)"
-REGION="${AZ%?}"
-INSTANCE_ID="$(curl -sL 169.254.169.254/latest/meta-data/instance-id)"
-
-aws ec2 modify-instance-attribute --region $REGION --instance-id i-$INSTANCE_ID --source-dest-check '{"Value": true}'
