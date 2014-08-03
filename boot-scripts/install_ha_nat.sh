1bcd5aea05cb40232072ddafb61cb95d092e6305
Paul Allen <paul.allen@lithium.com>
2014-08-03 15:17:29 -0500

    install the ha-nat package

--- boot-scripts/install_ha_nat.sh
+++ boot-scripts/install_ha_nat.sh
@@ -6,6 +6,7 @@
 ##   - PRIVATE_SUBNETS
 
 (
+    yum install -y ha-nat
     cd /opt/
     nohup ./ha-nat.py --monitor-interval $MONITORINTERVAL --log-file '$LOGFILE' --private-subnets '$PRIVATE_SUBNETS' &
     echo "* * * * * ps -fwwC python | grep -q ha-nat || { cd /opt/; nohup ./ha-nat.py --monitor-interval $MONITORINTERVAL --log-file '$LOGFILE' --private-subnets '$PRIVATE_SUBNETS' & " | crontab
